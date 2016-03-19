9609bfa46d57161fd48b2c2ddc53661d4355346c
AJP <allenatmc@gmail.com>
2016-03-19 15:55:03 -0500

    cors

--- app/index.js
+++ app/index.js
@@ -23,10 +23,12 @@ let db = mongoose.connection;
 
 //middleware to allows CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Credentials', true)
     console.log('cors middleware!')
+    console.log(res.header())
     next();
 })
 
