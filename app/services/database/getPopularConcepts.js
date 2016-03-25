1134aad5b75ac89c087483866ccc1236372def85
AJP <allenatmc@gmail.com>
2016-03-25 14:08:21 -0500

    fix popular concepts

--- app/services/database/getPopularConcepts.js
+++ app/services/database/getPopularConcepts.js
@@ -10,14 +10,45 @@ module.exports = (callback) => {
   let data = {}
 
   Concept.find()
-    .sort('label')
-    .select('label _id')
+    .sort('-size')
+    .limit(40)
     .exec((err, collection)=>{
        if (err) throw err
 
 
-      callback(collection)
+
+      let bunchOfCallBacks = [];
+
+      collection.forEach((concept)=>{
+
+        data[concept.label] = {}
+        data[concept.label]['articles'] = []
+
+        concept.articles.forEach((articleId)=>{
+
+          let newPromise = new Promise((resolve, reject) => {
+
+            Article.findOne({_id: articleId})
+            .exec((err, article)=>{
+              if (err) {reject(); throw err}
+
+              if (article) {
+                  data[concept.label]._id = concept._id
+                  data[concept.label].articles.push({
+                  url: article.url,
+                  title: article.title,
+                  id: article._id
+                })
+              }
+              resolve()
+            })
+          }) //end promise
+          bunchOfCallBacks.push(newPromise)
+        }) //end for each
+      }) //end for each
+
+    Promise.all(bunchOfCallBacks).then(()=>{
+      callback(data)
+    });
     }) // end find
 } //end exports
-
-
