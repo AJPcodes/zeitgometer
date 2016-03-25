2816e9009d95b8badafb2d365b6e87a3139d2b0d
AJP <allenatmc@gmail.com>
2016-03-25 12:02:53 -0500

    delete all but most recent trends

--- app/services/database/getTrending.js
+++ app/services/database/getTrending.js
@@ -9,14 +9,21 @@ module.exports = (callback, res) => {
   Trending.findOne()
     .sort({'date': -1})
     .limit(1)
-    .exec((err, articles)=>{
+    .exec((err, article)=>{
       if (err) console.log(err)
 
-      if (!articles) {
+        console.log(article._id)
+        Trending.find({_id: {$ne: article._id}})
+          .remove()
+          .exec((err, oldTrends) => {
+            if (err) console.log(err)
+          })
+
+      if (!article) {
         res.status(204).send({data: null})
       } else {
 
-         callback(articles);
+         callback(article);
       } //end else
   }) // end find
 
